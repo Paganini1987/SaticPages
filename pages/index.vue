0567d771885d7a3e48cdeaf4fa6adf11e94e75b3
Pavel <webmkirov@gmail.com>
2020-05-02 17:33:09 +0300

    added text

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,9 @@
 					</div>
 				</div>
 			</div>
+			<div class="col-md-12">
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae officiis deleniti mollitia at magni quo excepturi asperiores cupiditate, ab illum suscipit reiciendis maxime magnam ipsa debitis ea necessitatibus distinctio itaque! Fugit, ad voluptas ex molestias rerum saepe et mollitia ratione amet voluptatibus repellat officiis nisi sapiente tenetur facere harum labore maiores, corrupti cumque dolorum eligendi delectus. Eaque, soluta odio. Praesentium, excepturi modi fugit optio enim voluptas aut eaque neque libero quis. Assumenda veritatis praesentium repellendus neque ullam ea quia. Quae ipsam a eaque, tempore dolores ex facere voluptatum ab eveniet voluptatibus blanditiis quod nobis nisi quidem. Quae repellat repellendus assumenda ab, autem obcaecati! Exercitationem, cum suscipit unde illo esse blanditiis rem non? Tenetur molestias eos quaerat aliquam consequatur sed? Hic dignissimos praesentium asperiores, optio nesciunt rem sequi provident omnis facilis repudiandae, labore, reiciendis vel ex voluptatem doloremque laborum quo repellendus earum totam. Soluta temporibus inventore molestias illo ratione, voluptatibus aut natus quia reprehenderit rerum nostrum, odit cumque distinctio expedita neque sunt aperiam ipsam id ex et. Animi architecto exercitationem molestias blanditiis nostrum aliquid perferendis dicta nisi labore cumque, necessitatibus laudantium? Vel alias animi earum doloribus quia facilis ducimus? Incidunt perferendis maxime laboriosam doloremque omnis id et. Velit optio rerum ut?
+			</div>
 		</div>
 	</div>
 </template>
