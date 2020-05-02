f3eb3847e95bc6b4252f72aa0cf831d6bc5fe9c7
Pavel <webmkirov@gmail.com>
2020-05-02 17:40:16 +0300

    added posts page

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,26 +1,23 @@
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-					<a class="navbar-brand" href="#">Navbar</a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-					</button>
-					<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav">
-						<li class="nav-item active">
-							<nuxt-link exact active-class="active" to="/" class="nav-link" href="#">Home</nuxt-link>
-						</li>
-						<li class="nav-item active">
-							<nuxt-link active-class="active" to="/about" class="nav-link" href="#">About</nuxt-link>
-						</li>
-					</ul>
-					</div>
-				</nav>
-			</div>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">Navbar</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+		<ul class="navbar-nav">
+			<li class="nav-item active">
+				<nuxt-link exact active-class="active" to="/" class="nav-link" href="#">Home</nuxt-link>
+			</li>
+			<li class="nav-item active">
+				<nuxt-link active-class="active" to="/posts" class="nav-link" href="#">Posts</nuxt-link>
+			</li>
+			<li class="nav-item active">
+				<nuxt-link active-class="active" to="/about" class="nav-link" href="#">About</nuxt-link>
+			</li>
+		</ul>
 		</div>
-	</div>
+	</nav>
 </template>
 
 <script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Posts Page</h1>
+
+				<div class="card-columns mt-4">
+					<div class="card"  v-for="post in posts" :key="post.id">
+						<div class="card-body">
+							<h5 class="card-title">{{ post.title }}</h5>
+							<p class="card-text">{{ post.body }}</p>
+							<!-- <a href="#" class="btn btn-primary"  @click.prevent="openPost(post)">Go somewhere</a> -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	async asyncData ({$axios}) {
+		const posts = await $axios.$get('https://jsonplaceholder.typicode.com/posts')
+
+		return {posts}
+	},
+	data () {
+		return {
+			posts: []
+		}
+	},
+	methods: {
+		openPost (post) {
+			this.$router.push('/posts/' + post.id)
+		}
+	}
+
+}
+</script>
+
+<style>
+
+</style>
